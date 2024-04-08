31169f1478712497be75369e2ae2606cbedfbfe6
y1g4 <yfrank259@gmail.com>
2024-04-08 12:39:58 +0300

    users added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request,'blog/home.html')
-
-def about(request):
-    return render(request,'blog/about.html')
-
 posts = [
     {
         'author': 'Frank',
@@ -21,6 +15,16 @@ posts = [
         'dete_posted': 'April 02,2024'
     }
 ]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
+
+def about(request):
+    return render(request,'blog/about.html')
+
 
 
 # Create your views here.
